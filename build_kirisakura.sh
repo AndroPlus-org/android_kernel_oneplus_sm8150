8c507c3b3053735688c1c49c55d979239508984f
AndroPlus <mail@andro.plus>
2019-06-01 20:58:37 +0900

    Fix build and script

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -4,7 +4,7 @@ echo
 echo "Clean Build Directory"
 echo 
 
-make clean && make mrproper
+#make clean && make mrproper
 
 echo
 echo "Issue Build Commands"
@@ -13,10 +13,10 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983c/bin
+export CLANG_PATH=~/bin/linux-x86/clang-r353983c/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/bin/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 echo
 echo "Set DEFCONFIG"
@@ -28,3 +28,5 @@ echo "Build The Good Stuff"
 echo 
 
 make CC=clang O=out -j4
+
+mkdtimg create ../dtbo.img --page_size=4096 `find out/arch/arm64/boot/dts -name "*.dtbo"`
